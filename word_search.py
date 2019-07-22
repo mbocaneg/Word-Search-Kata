4cf41c9a05060055689f678d2543a8ae4bc4fb80
mbocaneg <mbocaneg@hawk.iit.edu>
2019-07-21 22:27:20 -0500

    documented new function

--- word_search.py
+++ word_search.py
@@ -94,24 +94,73 @@ class WordSearch:
                     for dir in self.directions:
                         self.scan_word(word, y, x, dir)
 
+
+    def scan_word(self, word, y, x, direction):
+        """Function that given and x,y point, scans 8 directions(N, NE, E, SE, S, SW, W, NW)
+        to see if a given word is present
+
+        Parameters
+        ----------
+        word: str
+            word we wish to find
+        y: int
+            y coordinate of the point we wish to examine
+        x: int
+            x coordinate of the point we wish to examine
+        direction: list of int
+            two value list that represents a cardinal direction that
+            we wish to explore(a list of directions is defined as the class member, 'directions')
+            the first element represents an offset in the y direction, and the second element represents
+            an offset in the x direction. These offsets take the value -1, 0, or 1. The cardinal directions
+            are represented as follows:
+            [-1,  0] NORTH
+            [-1,  1] NORTHEAST
+            [ 0,  1] EAST
+            [ 1,  1] SOUTHEAST
+            [ 1,  0] SOUTH
+            [ 1, -1] SOUTHWEST
+            [ 0, -1] WEST
+            [-1. -1] NORTHWEST
+        
+        """
+
+        # extract x and y offsets from the direction list
+        y_offset, x_offset = direction
+
+        # # initialize an empty list of coordinates for the potential word location
+        coordinates = []
+
+        # for each character in the word
+        for i, char in enumerate(word):
+
+            # compute the x and y coordinates of the point we wish to explore using offsets
+            x_i = x + i * x_offset
+            y_i = y + i * y_offset
+
+            # if the computed point is out of bounds, return
+            if(x_i < 0 or x_i >= self.dims or y_i < 0 or y_i >= self.dims):
+                return
+
+            # if the letter at the computed point equals the letter of the word we are looking for
+            # append the current point to the coordinates list
+            if(self.board[y_i][x_i] == char):
+                coordinates.append([x_i, y_i])
+
+        # finally, if the length of the coordinates list is equal to length of the word
+        # we were looking for, append the coordinates we found, along with the word,
+        # to the answers dictionary
+        if(len(coordinates) == len(word)):
+            self.answers[word] = coordinates
+
+    
+    """Function that prints out the answers dictionary. It prints out the word, along
+    with the coordinates of its letters found in the game board
+
+    """
     def print_solutions(self):
         for word in self.answers:
             print(word)
             print(*self.answers[word])
-
-    def scan_word(self, word, y, x, direction):
-        y_offset, x_offset = direction
-        coordinates = []
-        for i, char in enumerate(word):
-            x_i = x + i * x_offset
-            y_i = y + i * y_offset
-
-            if(x_i < 0 or x_i >= self.dims or y_i < 0 or y_i >= self.dims):
-                return
-            if(self.board[y_i][x_i] == char):
-                coordinates.append([x_i, y_i])
-        if(len(coordinates) == len(word)):
-            self.answers[word] = coordinates
 
 
     def find_word_north(self, word, y, x):
